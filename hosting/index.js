7f8052cbb1f71827f24769a2e48905b1a068b34d
Ethan Wallace <ethan.wallace.91@gmail.com>
2021-04-27 10:17:28 -0400

    Updated express settings

--- hosting/index.js
+++ hosting/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
+const helmet = require("helmet");
 var server = express();
+
+server.use(helmet.noSniff());
+server.use(helmet.hidePoweredBy());
+server.use(helmet.frameguard());
+
 var options = { index: 'index.html' };
 
 server.use('/', express.static('/home/site/wwwroot', options));
 server.use('/home', express.static('/home/site/wwwroot', options));
 server.use('/accueil', express.static('/home/site/wwwroot', options));
+
+server.use((req, res) => {
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+});
 
 server.get('*', (req, res) => {
     res.redirect('/');
